73d401871c53d931e834d720886197ff2515ecf7
Suborno <112712723+Suborno22@users.noreply.github.com>
2023-11-24 18:42:25 +0530

    Update server.php

--- server.php
+++ server.php
@@ -14,18 +14,39 @@ $range = 'Sheet1';
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $rows = $response->getValues();
 
+$headers = array_shift($rows);
 
-?>
+// var_dump($headers);
+
+$array = [];
+foreach ($rows as $row) {
+    // Check if $row is not NULL
+    if ($row !== null) {
+        if (count($headers) === count($row)) {
+            $array[] = array_combine($headers, $row);
+        } else {
+            // Handle the case where the number of elements is not the same
+            // You can log an error, skip this row, or handle it based on your requirements
+            // echo "Number of elements in headers and row are not the same for this row\n";
+        }
+    } else {
+        // Handle the case where $row is NULL
+        // echo "Row is NULL\n";
+    }
+}
+
+$jsonString = json_encode($array, JSON_PRETTY_PRINT);
+print($jsonString);
+
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Movie Data</title>
 </head>
 <body>
-    <h1>Lists of movies</h1>
     <table border="1">
         <thead>
             <tr>
